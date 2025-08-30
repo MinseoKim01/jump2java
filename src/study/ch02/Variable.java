8db38dde8aca9689c7d22fcc21ffa348989e4a2a
MinseoKim01 <202513341@pukyong.ac.kr>
2025-08-30 21:01:50 +0900

    java 변수명 연습하기

---
+++ src/study/ch02/Variable.java
@@ -0,0 +1,13 @@
+/* 패키지는 자바에서 클래스를 묶는 폴더 개념, 패키지를 쓰면 폴더별로 구분해서 정리할 수 있음
+꼭 적어야하는 것은 아니며, 팀프로젝트나 실무 코드에서는 작성하는 것이 좋음
+*/
+// 즉 아래 문장은 이 클래스는 study 폴더 안의 ch02 폴더에 들어있다는 것을 알려주는 주소 같은 것
+package study.ch02;
+
+public class Variable {
+    public void runFast() {}
+    public void variablePractice() {
+        int i;
+        char c;
+    }
+}
--- src/study/ch02/variable.java
+++
@@ -1,4 +0,0 @@
-package study.ch02;
-
-public class variable {
-}
